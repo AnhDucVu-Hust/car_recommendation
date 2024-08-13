feb33597e1618b9ffe00539cc597cb567276b29f
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2024-08-13 20:06:31 +0700

    Update chains.py

--- src/chatbot/chains.py
+++ src/chatbot/chains.py
@@ -20,7 +20,8 @@ def follow_up_chain():
 def get_follow_up(question,chat_history):
     chain = follow_up_chain()
     query = chain.invoke({"question":question,"chat_history":merge_chat_history(chat_history)})
-    query = json.loads(query.replace("\n","").strip().replace("\\",""))
+    query = query.replace("\n","").strip().replace("\\","")
+    query = json.loads(query)
     return query['question']
 def input_chain():
     input_handle_prompt = utils.get_prompt("input_guardrail")
