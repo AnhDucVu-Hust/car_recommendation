1b33932b3c7578fd04344c01a7c719b4994ca2c4
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2024-08-13 20:22:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,10 @@ def main():
         with st.chat_message("user"):
             st.markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
-        response,_ = answer_with_rag(question=st.session_state.messages[-1]["content"],chat_history=st.session_state.messages[:-1])
+        if len(st.session_state.messages>3):
+            response,_ = answer_with_rag(question=st.session_state.messages[-1]["content"],chat_history=st.session_state.messages[:-1])
+        else:
+            response,_ = answer_with_rag(question=st.session_state.messages[-1]["content"],chat_history=st.session_state.messages[-4:-1])
         response = response.replace("$","\$")
         with st.chat_message("assistant"):
             st.write(response, unsafe_allow_html=True)
