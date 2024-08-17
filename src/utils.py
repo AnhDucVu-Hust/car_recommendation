3f01fad75727b3301c2d3fa51152f7e6ee2db914
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2024-08-17 19:12:27 +0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ def get_current_folder():
     return str(Path(__file__).parent)
 def get_llm():
     os.environ["OPENAI_API_KEY"] = openai_key
-    llm = ChatOpenAI(model="gpt-4o-mini",temperature=0)
+    llm = ChatOpenAI(model="gpt-4o",temperature=0)
     return llm
 def get_prompt(file_name):
     with open(get_current_folder()+f'/prompt_template/{file_name}.yaml',encoding="UTF-8") as f:
